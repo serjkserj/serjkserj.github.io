017ffd03dab05c588d4b230f0f742c1261c19b98
S S <rsobr76@gmail.com>
2026-03-22 22:19:00 +0300

    Sync handbook waves 1-3 and residual sweep

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   organizationName,
   projectName,
   deploymentBranch: 'main',
-  trailingSlash: true,
+  trailingSlash: false,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   future: {
@@ -43,32 +43,7 @@ const config = {
       }),
     ],
   ],
-  plugins: [
-    [
-      require.resolve('@easyops-cn/docusaurus-search-local'),
-      {
-        indexDocs: true,
-        indexBlog: false,
-        indexPages: false,
-        docsRouteBasePath: ['handbook'],
-        language: ['ru', 'en'],
-        hashed: true,
-        removeDefaultStemmer: true,
-        searchResultLimits: 12,
-        searchResultContextMaxLength: 100,
-        highlightSearchTermsOnTargetPage: true,
-        explicitSearchResultPath: true,
-        searchBarShortcut: true,
-        searchBarShortcutHint: false,
-        ignoreFiles: [
-          '/handbook/appendices/CHECKLIST_TEMPLATE',
-          '/handbook/appendices/REMARK_TEMPLATE',
-        ],
-      },
-    ],
-  ],
   themes: ['@docusaurus/theme-mermaid'],
-  scripts: [{src: '/js/diagram-viewer.js', defer: true}],
   themeConfig:
     ({
       image: 'img/handbook-mark.svg',
@@ -96,7 +71,6 @@ const config = {
             label: 'Приложения',
             position: 'left',
           },
-          {type: 'search', position: 'right'},
           {
             href: 'https://github.com/serjkserj/bim-handbook',
             label: 'GitHub',
@@ -140,4 +114,3 @@ const config = {
 };
 
 export default config;
-
